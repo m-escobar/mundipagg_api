f91885a0c06daf264733d8b7f70971db73f59007
Marcelo Escobar <escobar@br.systems>
2019-06-18 16:58:17 -0300

    authentication problems

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,14 +1,30 @@
 class PaymentController < ApplicationController
     def index
         # Configuration parameters and credentials
-        basic_auth_user_name = 'pk_test_dpqm6W4Fo3uDe95n' # The username to use with basic authentication
-        basic_auth_password = 'sk_test_ldXjepulPivN39QD' # The password to use with basic authentication
+        basic_auth_user_name = Base64.encode64('acc_6W1Dm5ef9WcpxDjz') # The username to use with basic authentication
+        basic_auth_password = Base64.encode64('sk_test_ldXjepulPivN39QD') # The password to use with basic authentication
         
 
         client = MundiApi::MundiApiClient.new(
           basic_auth_user_name: basic_auth_user_name,
           basic_auth_password: basic_auth_password
         )
-        raise
-    end
+
+        
+        customers_controller = client.customers
+        
+        
+        request = MundiApi::CreateCustomerRequest.new
+        
+        request.name = 'Marcelo Escobar'
+        request.email = 'marceloescobar.s@gmail.com'
+        request.type = 'indivudual'
+
+        result = customers_controller.create_customer(request)
+        
+
+
+raise
+      end
+
 end
