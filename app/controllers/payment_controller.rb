1946d60af31eddbe268f78418492e997c03b9138
Marcelo Escobar <escobar@br.systems>
2019-07-09 20:34:52 -0300

    all working

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,72 +1,76 @@
 class PaymentController < ApplicationController
+  before_action :login, only: [:index, :create_user]
+
   def index
-    # Configuration parameters and credentials
-    basic_auth_user_name = ENV['Mundi_API'] # The username to use with basic authentication
-    basic_auth_password = ('') # The password to use with basic authentication
+    @result = @customers_controller.get_customers
+  end
 
-    client = MundiApi::MundiApiClient.new(
-      basic_auth_user_name: basic_auth_user_name,
-      basic_auth_password: basic_auth_password
-    )
-
-    customers_controller = client.customers
-    
-    request = MundiApi::CreateCustomerRequest.new
-    
+  def create_user
     request.name = 'Sandra Escobar S'
     request.email = 's.escobar@gmail.com'
     request.type = 'individual'
 
-    result = customers_controller.create_customer(request)
     raise
+    @result = @customers_controller.create_customer(request)
   end
 
   def create_all_plans
     # Configuration parameters and credentials
     basic_auth_user_name = ENV['Mundi_API'] # The username to use with basic authentication
     basic_auth_password = ('') # The password to use with basic authentication
-
-
+    
+    
     client = MundiApi::MundiApiClient.new(
       basic_auth_user_name: basic_auth_user_name,
       basic_auth_password: basic_auth_password
-    )
+      )
+      
 
-    plans_controller = client.plans
+      plans_controller = client.plans
+      
+      request = MundiApi::CreatePlanRequest.new
+      
+      #Not in use but sintax is correct
+      #-------------
+      #Use to include an item
+      # request.items = [{
+      #   "name": "Mensal",  
+      #   "quantity": 1,
+      #   "pricing_scheme": {
+      #     "price": 2540,  
+      #     "scheme_type": "unit"
+      #   }
+      # }]
+      #
+      #-------------
+      #To include billing_days
+      # request.billing_days = [1, 20]  #dont set when using prepaid
+      #-------------
+      #To include Metadata
+      # request.metadata = {
+      #   "id": "my_plan_id"  
+      # }
+      #-------------
+      #To include others
+      # request.cycles = 1
+      # request.minimum_price = 10000
+      # request.shippable = false
+      
+      #Creating Montly Plan
+      request.name = 'Plano Mensal'
+      request.description = 'Assinatura Mensal'
+      request.statement_descriptor = 'Cobranca Mensal'
+      request.payment_methods = ['credit_card']
+      request.installments = [1]
+      request.currency = 'BRL'
+      request.interval = 'month'
+      request.interval_count = 1
+      request.billing_type = 'prepaid'
+      request.pricing_scheme = { "price": 2540 }
+      request.quantity = 1
+      request.trial_period_days = 7
 
-    request = MundiApi::CreatePlanRequest.new
-    
-    request.name = 'Plana'
-    request.description = 'Assinatura Mensal'
-    request.statement_descriptor = 'teste'
-    request.items = [{
-      "name": "Bodybuilding",
-      "quantity": 1,
-      "pricing_scheme": {
-        "price": 18990,
-        "scheme_type": "unit"
-      }
-    }]
-    request.shippable = false
-    request.payment_methods = ['credit_card']
-    request.installments = [1]
-    request.currency = 'BRL'
-    request.interval = 'month'
-    request.interval_count = 1
-    request.billing_days = [1, 20]
-    request.billing_type = 'exact_day'
-    request.pricing_scheme = { "price": 18990 }
-    request.metadata = {
-      "id": "my_plan_id"
-    }
-    request.minimum_price = 10000
-    request.cycles = 1
-    request.quantity = 1
-    request.trial_period_days = 7
-
-    raise
-    result = plans_controller.create_plan(request)
-    raise
+      @result = plans_controller.create_plan(request)
   end
 
   def get_plans
@@ -115,5 +119,18 @@ raise
 
     result = customers_controller.create_address(customer_id, request)
   raise
+  end
+
+  private
+  def login
+    # Configuration parameters and credentials
+    basic_auth_user_name = ENV['Mundi_API'] # The username to use with basic authentication
+    basic_auth_password = ('') # The password to use with basic authentication
+
+    client = MundiApi::MundiApiClient.new(
+      basic_auth_user_name: basic_auth_user_name,
+      basic_auth_password: basic_auth_password
+    )
+    @customers_controller = client.customers
   end
 end
