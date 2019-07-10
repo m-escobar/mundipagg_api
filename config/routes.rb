7274619c59b6e3b861447223917562be67e97ffd
Marcelo Escobar <escobar@br.systems>
2019-07-10 14:57:45 -0300

    doing this api

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #Index with info
   root to: 'pages#index'
 
   get 'pages/index', as: 'index-pt'
   get 'pages/index-en', as: 'index-en'
-  get 'pay', to: 'payment#index'
-  get 'create_all_plans', to: 'payment#create_all_plans'
-  get 'get_plans', to: 'payment#get_plans'
-  get 'address', to: 'payment#create_address'
+
+  #for testing only
+  get 'create_plans', to: 'testing#create_plans'
+  get 'get_plans', to: 'testing#get_plans'
+  
+  get 'customer', to: 'customer#main'
+  get 'pay', to: 'payment#main'
 end
