4e207df31d9c84fb673d3d7c04236081a3c5779e
Marcelo Escobar <escobar@br.systems>
2019-07-11 16:16:22 -0300

    befire start subscription

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,118 +1,227 @@
 class CustomersController < ApplicationController
-    before_action :login, only: [:main]
+  before_action :login, only: [:main]
     
-    def initialize
-        @hash =  { "cliente": {
-                    "nome": "Maria Santos",
-                    "email": "mariosantos@gmail.com"
-                },
-                "cartao": {
-                    "numero": "4584441896453869",
-                    "expiracao_mes": 12,
-                    "expiracao_ano": 2019,
-                    "cvv": "591"
-                },
-                "produtos": [
-                    {
-                        "tipo": "plano mensal",
-                        "plano_id": "plan_w92xZgaSRPiAWE8N"
+  def initialize
+    @hash =  { "operacao": {
+                "tipo": "create"
+                }, 
+  
+              "cliente": {
+                # "cliente_id": "cus_5MlXRjBFL4heWXxE",
+                "nome": "Mariana Santos",
+                "email": "marionasantos@gmail.com"
+              },
+              "cartao": {
+                  "numero": "4584441896453869",
+                  "expiracao_mes": 12,
+                  "expiracao_ano": 2019,
+                  "cvv": "591"
+              },
+              "produtos": [
+                  {
+                      "tipo": "plano Teste 123",
+                      "plano_id": "plan_w92xZgaSRPiAWE8N",
+                      "nome": "plano Teste 123",
+                      "descricao": "Assinatura Mensal",
+                      "info_extrato": "Cobranca Mensal",
+                      "metodo_pagamento": "credit_card",
+                      "parcelas": 1,
+                      "moeda": "BRL",
+                      "tipo_intervalo": "month",
+                      "intervalo": 1,
+                      "tipo_cobranca": "prepaid",
+                      "valor": 2540,
+                      "periodo_teste": 7,
+                      "quantidade": 1
                     }
-                ]
-            }
+                ],
+              "endereco": {
+                    "rua": "Rua Victor",
+                    "numero": "479",
+                    "bairro": "Jardim Maralina",
+                    "cep": "06704-505",
+                    "cidade": "Cotia",
+                    "estado": "SP",
+                    "pais": "BR"
+                  }
+          }
+  end
+
+  def main
+    unless @hash[:operacao][:tipo].nil? then
+      @to_return = ""
+      case @hash[:operacao][:tipo]
+        when "list"
+          op_list
+        when "create"
+          op_create
+        when  "update"
+          op_update
+        when "destroy"
+          op_destroy
+        else @to_return = {"error": "Invalid Operation"}
+      end
+    else 
+      @to_return = {"error": "Undefined Operation"}
     end
 
-    def main
-      #customer
-      unless @hash[:cliente].nil? 
-        user =  MundiApi::CreateCustomerRequest.new
+      raise
+    return @to_return
+  end
+
+  def op_list
+  end
+  
+  def op_create
+    #customer
+    user =  MundiApi::CreateCustomerRequest.new
+    unless @hash[:cliente].nil?
+      unless @hash[:cliente][:cliente_id].nil?
+        user = @customers_controller.get_customer(@hash[:cliente][:cliente_id])
+      else
         user = create_user(@hash[:cliente])
-        if user.nil?
-          return {"error": "User creation error"}
-        else 
-          @user = user
-        end
       end
       
-      #cartao
-      unless @hash[:cartao].nil? 
-        card =  MundiApi::CreateCardRequest.new
-        card = create_card(user.id, @hash[:cartao])
-        if card.nil?
-          return {"error": "Card creation error"}
-        else 
-          @card = card
+      @user = user.nil? ? {"error": "User creation error"} : user
+    end #end customer
+
+    #cartao
+    unless @hash[:cartao].nil? 
+      card =  MundiApi::CreateCardRequest.new
+      card = create_card(@user.id, @hash[:cartao])
+      @card = card.nil? ? {"error": "Card creation error"} : card
+    end #end cartao
+
+    #address
+    address =  MundiApi::CreateCustomerRequest.new
+      unless @hash[:endereco].nil?
+        unless @user.nil?
+          address = create_address(@user.id, @hash[:endereco])
+        else
+            return {"error": "Undefined Customer"}
+        end
+      @address = address.nil? ? {"error": "Address creation error"} : address
+    end #end address
+
+    #product
+    unless @hash[:produtos].nil? 
+      @products = []
+      @hash[:produtos].each do |produto|
+        product = MundiApi::CreatePlanRequest.new
+        product = create_plan(produto)
+        if product.nil? 
+          @products << {"error": "Plan creation error"}
+        else
+          @products << product
         end
       end
+    end #end product
 
-      #cria endereço
-      create_address
+    #subscription
+    unless @hash[:cliente].nil? && @hash[:cartao].nil? && @hash[:produtos].nil? 
 
-      #produto
-      unless @hash[:produtos].nil? 
-        subscription = MundiApi::CreateSubscriptionRequest.new
-        subscription = create_subscription(user.id, @hash)
-        if subscription.nil?
-          return {"error": "Subscription creation error"}
-        else 
-          @subscription = subscription
-        end
-        raise
-      end
+      subscription = MundiApi::CreateSubscriptionRequest.new
+      subscription = create_subscription(user.id, @plan, @card)
+      @subscription = subscription.nil? ? {"error": "Subscription creation error"} : subscription
+      raise
+    end #end subscription
+
+  end
+   
+private
+  def login
+    # Configuration parameters and credentials
+    basic_auth_user_name = ENV['Mundi_API'] # The username to use with basic authentication
+    basic_auth_password = ('') # The password to use with basic authentication
+
+    client = MundiApi::MundiApiClient.new(
+      basic_auth_user_name: basic_auth_user_name,
+      basic_auth_password: basic_auth_password
+    )
+    @customers_controller = client.customers
+    @subscriptions_controller = client.subscriptions
+    @plans_controller = client.plans
+  end
+
+  def create_user(customer)
+    request =  MundiApi::CreateCustomerRequest.new
+    request.name = customer[:nome]
+    request.email = customer[:email]
+    request.type = 'individual'
+    
+    begin
+      result = @customers_controller.create_customer(request)
+    rescue => e
+      result = nil
     end
+    return result
+  end
   
-    def create_user(client)
-      request =  MundiApi::CreateCustomerRequest.new
-      request.name = client[:nome]
-      request.email = client[:email]
-      request.type = 'individual'
+  def create_card(customer_id, card)
+    request =  MundiApi::CreateCardRequest.new
+    request.holder_name = !card[:nome].nil? ? card[:nome] : @hash[:cliente][:nome]
+    request.number = card[:numero]
+    request.exp_month = card[:expiracao_mes]
+    request.exp_year = card[:expiracao_ano]
+    request.cvv = card[:cvv]
+    
+    begin
+      result = @customers_controller.create_card(customer_id, request)
+    rescue => e
+      result = nil
+    end
+    return result
+  end
 
-      begin
-        result = @customers_controller.create_customer(request)
-      rescue => e
-        # This is the same as rescuing StandardError
-        result = nil
-      end
-      
-      return result
+  def create_address(customer_id, address)
+    request =  MundiApi::CreateAddressRequest.new
+    request.line_1 = "#{address[:numero]}, #{address[:rua]}, #{address[:bairro]}"
+    request.zip_code = "#{address[:cep]}"
+    request.city = address[:cidade]
+    request.state = address[:estado]
+    request.country = address[:pais]
+
+    begin
+      result = @customers_controller.create_address(customer_id, request)
+    rescue => e
+      result = nil
     end
-  
-    def create_card(customer_id, card)
-      request =  MundiApi::CreateCardRequest.new
-      request.holder_name = !card[:nome].nil? ? card[:nome] : @hash[:cliente][:nome]
-      request.number = card[:numero]
-      request.exp_month = card[:expiracao_mes]
-      request.exp_year = card[:expiracao_ano]
-      request.cvv = card[:cvv]
-      
-      begin
-        result = @customers_controller.create_card(customer_id, request)
-      rescue => e
-        # This is the same as rescuing StandardError
-        result = nil
-      end
-      return result
+    return result
+  end #end create_address
+
+  def create_plan(produto)
+    request = MundiApi::CreatePlanRequest.new
+    request.name = produto[:nome]
+    request.description = produto[:descricao]
+    request.statement_descriptor = produto[:info_extrato]
+    request.payment_methods = [produto[:metodo_pagamento]]
+    request.installments = [produto[:parcelas]]
+    request.currency = produto[:moeda]
+    request.interval = produto[:tipo_intervalo]
+    request.interval_count = produto[:intervalo]
+    request.billing_type = produto[:tipo_cobranca]
+    request.pricing_scheme = { "price": produto[:valor] }
+    request.quantity = produto[:quantidade]
+    request.trial_period_days = produto[:periodo_teste]
+
+    begin
+      result = @plans_controller.create_plan(request)
+    rescue => e
+      result = nil
     end
- 
-    def create_subscription(client, info)
-      request =  MundiApi::CreateSubscriptionRequest.new
-# raise
-      # request.customer = @user
-#      request.customer_id = @user.id
-#       request.card = @card
-#       request.plan_id = info[:produtos][0][:plano_id]
-#       request.payment_method = 'credit_card'
-#       request.billing_type = 'prepaid'
-#       request.statement_descriptor = 'Cobranca Mensal'
-#       request.description = 'Assinatura Mensal'
-#       request.currency = 'BRL'
-#       request.interval = 'month'
-#       request.interval_count = 1
-#       request.pricing_scheme = { "price": 2540 }
-#       request.items = 
-# request.name = 'Plano Mensal'
-#     request.installments = [1]
-#     request.quantity = 1
-#     request.trial_period_days = 7
+    return result
+  end #end create_plan
+
+  def create_subscription(customer_id, plan, card)
+    request =  MundiApi::CreateSubscriptionRequest.new
+    # request.plan_id = info[:produtos][0][:plano_id]
+    # request.statement_descriptor = 'Cobranca Mensal'
+    # request.description = 'Assinatura Mensal'
+    # request.pricing_scheme = { "price": 2540 }
+    # request.name = 'Plano Mensal'
+    # request.quantity = 1
+    # request.trial_period_days = 7
+    # request.boleto_due_days=5
 
     request.payment_method = "credit_card"
     request.currency= "BRL"
@@ -120,82 +229,34 @@ class CustomersController < ApplicationController
     request.interval_count= 3
     request.billing_type= "prepaid"
     request.installments= 3
-    request.gateway_affiliation_id= "C56A4180-65AA-42EC-A945-5FD21DEC0538"
     request.minimum_price= 10000
-    request.boleto_due_days=5
     request.customer= {
         "name": "Tony Stark",
         "email": "tonystark@avengers.com"
     }
-    request.card = @card.id
-    # request.card= {
-    #     "holder_name": "Tony Stark",
-    #     "number": "4532464862385322",
-    #     "exp_month": 1,
-    #     "exp_year": 26,
-    #     "cvv": "903",
-    #     "billing_address": {
-    #         "line_1": "375, Av. General Justo, Centro",
-    #         "line_2": "8th floor",
-    #         "zip_code": "20021130",
-    #         "city": "Rio de Janeiro",
-    #         "state": "RJ",
-    #         "country": "BR"
-    #     }
-    #   }
+    request.card= {
+        "holder_name": "Tony Stark",
+        "number": "4532464862385322",
+        "exp_month": 1,
+        "exp_year": 26,
+        "cvv": "903"   
+    }
     request.items= [
         {
-            "description": "Bodybuilding",
-            "quantity": 1,
-            "pricing_scheme": {
-                "price": 18990
-            }
-        },
-        {
-            "description": "Registration",
-            "quantity": 1,
-            "cycles": 1,
-            "pricing_scheme": {
-                "price": 5990
-            }
+          "description": "Bodbuilding",
+          "quantity": 1,
+          "pricing_scheme": {
+            "price": 18990
+          }
         }
     ]
-      
 
-      raise
-      begin
-        result = @subscriptions_controller.create_subscription(request)
-      rescue => e
-        # This is the same as rescuing StandardError
-        result = nil
-      end
-      return result
+    begin
+      result = @subscriptions_controller.create_subscription(request)
+    rescue => e
+      result = nil
     end
- 
-    def create_address
-      request =  MundiApi::CreateAddressRequest.new
-      request.street = 'Rua Victor'
-      request.number = '479'
-      request.neighborhood  = 'vila dalva'
-      request.zip_code = '06704-505'
-      request.city = 'Cotia'
-      request.state = 'SP'
-      request.country = 'BR'
-  
-      @address = @customers_controller.create_address(@user.id, request)
-    end
+    return result
+  end #end create_subscription
 
-    private
-    def login
-      # Configuration parameters and credentials
-      basic_auth_user_name = ENV['Mundi_API'] # The username to use with basic authentication
-      basic_auth_password = ('') # The password to use with basic authentication
-  
-      client = MundiApi::MundiApiClient.new(
-        basic_auth_user_name: basic_auth_user_name,
-        basic_auth_password: basic_auth_password
-      )
-      @customers_controller = client.customers
-      @subscriptions_controller = client.subscriptions
-    end
-end    
+end #end class
